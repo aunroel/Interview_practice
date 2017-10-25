e756ce0b5f3795a842c143ad21ab96e8a5abf6e7
Roman Matios <rm252@st-andrews.ac.uk>
2017-10-25 17:07:27 +0100

    Stacks & queues

---
+++ src/stacks_queues/MyQueue.java
@@ -0,0 +1,45 @@
+package stacks_queues;
+
+import java.util.NoSuchElementException;
+
+public class MyQueue<T> {
+    private static class QueueNode<T> {
+        private T data;
+        private QueueNode<T> next;
+
+        QueueNode(T data) {
+            this.data = data;
+        }
+    }
+
+    private QueueNode<T> first;
+    private QueueNode<T> last;
+
+    public void add(T item) {
+        QueueNode<T> q = new QueueNode<T>(item);
+        if (last != null)
+            last.next = q;
+        last = q;
+        if (first == null)
+            first = last;
+    }
+
+    public T remove() {
+        if (first == null) throw new NoSuchElementException();
+        T item = first.data;
+        first = first.next;
+        if (first == null)
+            last = null;
+        return item;
+    }
+
+    public T peek() {
+        if (first == null) throw new NoSuchElementException();
+        return first.data;
+    }
+
+    public boolean isEmpty() {
+        return first == null;
+    }
+
+}
